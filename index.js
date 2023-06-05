9170fb6d5256ed5c4de122090b60785e67144602
Paulo Salvatore <salvatoregames@gmail.com>
2023-06-05 12:01:53 -0300

    Criação do endpoint `/oi`: `Olá, mundo!`

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000);
+app.get("/oi", function (req, res) {
+  res.send("Olá, mundo!");
+});
+
+app.listen(3000, function () {
+  console.log("Aplicando rodando em http://localhost:3000");
+});
