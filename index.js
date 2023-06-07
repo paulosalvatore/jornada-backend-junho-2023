74d4292d7beb9389d979f162c15b1d8afae6d541
Paulo Salvatore <salvatoregames@gmail.com>
2023-06-07 10:33:24 -0300

    `Read all` implementado

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get("/oi", function (req, res) {
   res.send("Olá, mundo!");
 });
 
+// Lista de heróis
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//              0                    1                2
+
+// Read All - [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(herois);
+});
+
 app.listen(3000, function () {
   console.log("Aplicando rodando em http://localhost:3000");
 });
